8b65ee92bfdf3c08fc5a611024544f4175fab72f
Guillem <guillemnaba@gmail.com>
2025-12-15 20:22:03 +0100

    Fix CORS allow all origins

--- TFM/config/cors.php
+++ TFM/config/cors.php
@@ -15,10 +15,10 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'http://localhost:3000'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
